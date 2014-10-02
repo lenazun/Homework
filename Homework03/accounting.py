5d33e0e19705d9c10dcf235d10e0aff496e1b48c
lenazun <lenazun@gmail.com>
2014-10-01 20:15:49 -0700

    First try, separates main into more functions

--- Homework03/accounting.py
+++ Homework03/accounting.py
@@ -1,5 +1,4 @@
-def main():
-    print "******************************************"
+def accounting():
     f = open("orders_by_type.csv")
     melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
     for line in f:
@@ -8,14 +7,17 @@ def main():
         melon_count = int(data[2])
         melon_tallies[melon_type] += melon_count
     f.close()
+    
     melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
     total_revenue = 0
+    
     for melon_type in melon_tallies:
         price = melon_prices[melon_type]
         revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
         print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    print "******************************************"
+
+def sales():
     f = open("orders_with_sales.csv")
     sales = [0, 0]
     for line in f:
@@ -30,6 +32,18 @@ def main():
         print "Guess there's some value to those salespeople after all."
     else:
         print "Time to fire the sales team! Online sales rule all!"
+
+
+
+def main():
+    print "******************************************"
+
+    accounting()
+
+    print "******************************************"
+
+    sales()
+
     print "******************************************"
 
 
